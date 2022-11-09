88a4604e99f0b24ac4d83aa3df0b4170030536d0
jimwei <jimwei6@gmail.com>
2022-11-08 21:08:53 -0800

    Added variation for get account

--- queries/queries.ts
+++ queries/queries.ts
@@ -1,4 +1,4 @@
-import pool from '../util/dbConnect.js';
+import pool from '../util/dbConnect';
 
 function executeQuery(query: string, values: string[] = []): any {
   return pool.query(query, values).then(res => res.rows);
@@ -8,9 +8,14 @@ function getParkingLots(): any {
   return executeQuery(`SELECT * FROM parkingLots`);
 }
 
-function getAccount(username: string, password: string): any {
-  return executeQuery(`SELECT * FROM accounts WHERE username = $1 AND password = $2`, [username, password]);
+function getAccount(username: string, password?: string): any {
+  if(password) {
+    return executeQuery(`SELECT * FROM accounts WHERE username = $1 AND password = $2`, [username, password]);
+  } else {
+    return executeQuery(`SELECT * FROM accounts WHERE username = $1`, [username]);
+  }
 }
+
 export default {
   getParkingLots,
   getAccount
